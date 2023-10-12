3e7d831dc16eece8f8c8badceda912559a07618f
jjkim0816 <jjkim0816@gmail.com>
2023-10-12 15:54:22 +0900

    test: 계좌 최초 생성, 해당 유저 없을 시 계좌 생성 실패, 유저 당 최대 계좌 10개 테스트 케이스

--- src/test/java/com/zerobase/account/service/AccountServiceTest.java
+++ src/test/java/com/zerobase/account/service/AccountServiceTest.java
@@ -3,6 +3,7 @@
  */
 package com.zerobase.account.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -20,8 +21,10 @@ import java.util.Optional;
 import com.zerobase.account.domain.Account;
 import com.zerobase.account.domain.AccountUser;
 import com.zerobase.account.dto.AccountDto;
+import com.zerobase.account.exception.AccountException;
 import com.zerobase.account.repository.AccountRepository;
 import com.zerobase.account.repository.AccountUserRepository;
+import com.zerobase.account.type.ErrorCode;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -67,5 +70,72 @@ class AccountServiceTest {
 		assertEquals(12L, accountDto.getUserId());
 //		assertEquals("1000000013", accountDto.getAccountNumber());
 		assertEquals("1000000013", accountCaptor.getValue().getAccountNumber());
+	}
+	
+	@Test
+	void createFirstAccount() {
+		// given
+		AccountUser user = AccountUser.builder()
+					.id(15L)
+					.name("Kobi").build();
+		
+		given(accountUserRepository.findById(anyLong()))
+			.willReturn(Optional.of(user));
+		
+		given(accountRepository.findFirstByOrderByIdDesc())
+			.willReturn(Optional.empty());
+		
+		given(accountRepository.save(any()))
+			.willReturn(Account.builder()
+					.accountUser(user)
+					.accountNumber("1000000015")
+					.build());
+		ArgumentCaptor<Account> accountCaptor = ArgumentCaptor.forClass(Account.class);
+		
+		// when
+		AccountDto accountDto = accountService.createAccount(1L, 1000L);
+		
+		// then
+		verify(accountRepository, times(1)).save(accountCaptor.capture());
+		assertEquals(15L, accountDto.getUserId());
+		assertEquals("1000000000", accountCaptor.getValue().getAccountNumber());
+	}
+	
+	@Test
+	@DisplayName("해당 유저 없음 - 계좌 생성 실패")
+	void createAccount_userNotFound() {
+		// given
+		given(accountUserRepository.findById(anyLong()))
+			.willReturn(Optional.empty());
+		
+		// when
+		AccountException exception = assertThrows(AccountException.class,
+				() -> accountService.createAccount(1L, 1000L));
+
+		// then
+		assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+	}
+	
+	@Test
+	@DisplayName("유저 당 최대 계좌는 10개")
+	void createAccount_maxAccountIs10() {
+		// given
+		AccountUser user = AccountUser.builder()
+				.id(12L)
+				.name("Pobi").build();
+		
+		
+		given(accountUserRepository.findById(anyLong()))
+			.willReturn(Optional.of(user));
+		
+		given(accountRepository.countByAccountUser(any()))
+			.willReturn(10);
+
+		// when
+		AccountException exception = assertThrows(AccountException.class,
+				() -> accountService.createAccount(1L, 1000L));
+		
+		// then
+		assertEquals(ErrorCode.MAX_COUNT_PER_USER_10, exception.getErrorCode());
 	}
 }
