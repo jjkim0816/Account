7cc82551c9d0b7392b2b00ef2c93260e1ccbb546
jjkim0816 <jjkim0816@gmail.com>
2023-10-19 19:37:47 +0900

    test: 중복거래 방지(AOP) test case

--- src/test/java/com/zerobase/account/controller/AccountControllerTest.java
+++ src/test/java/com/zerobase/account/controller/AccountControllerTest.java
@@ -18,7 +18,6 @@ import com.zerobase.account.dto.CreateAccount;
 import com.zerobase.account.dto.DeleteAccount;
 import com.zerobase.account.exception.AccountException;
 import com.zerobase.account.service.AccountService;
-import com.zerobase.account.service.RedisTestService;
 import com.zerobase.account.type.AccountStatus;
 import com.zerobase.account.type.ErrorCode;
 
@@ -38,9 +37,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AccountControllerTest {
 	@MockBean
 	private AccountService accountService;
-	
-	@MockBean
-	private RedisTestService redisTestService;
 	
 	@Autowired
 	private MockMvc mockMvc;
